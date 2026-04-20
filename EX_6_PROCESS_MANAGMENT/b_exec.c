2d5052edba70b8b521c8887e5b5dd71c237759bb
11249a150-art <11249a150@kanchiuniv.ac.in>
2026-04-20 20:18:52 +0530

    Update b_exec.c

--- EX_6_PROCESS_MANAGMENT/b_exec.c
+++ EX_6_PROCESS_MANAGMENT/b_exec.c
@@ -1,13 +1,4 @@
-/*b) Program using exec() System Call 
 
-Aim 
-To replace the current process image with a new process using the exec() system call. 
- 
-Algorithm 
-1. Create a child process using fork(). 
-2. Use exec() in the child process to execute a new program. 
-3. Parent process waits for the child. 
-Source Code*/ 
 #include <stdio.h> 
 #include <unistd.h> 
 #include <sys/wait.h> 
@@ -23,20 +14,3 @@ int main() {
     } 
     return 0; 
 } 
- 
- 
-/*Execution Procedure 
-1. Create file using vi exec.c. 
-2. Compile using gcc exec.c. 
-3. Execute using ./a.out. 
- 
-
-Sample Output 
-file1.txt 
-file2.txt 
-exec.c 
-Parent process completed 
- 
- 
-Result 
-Thus, a new program was successfully executed using the exec() system call. */
