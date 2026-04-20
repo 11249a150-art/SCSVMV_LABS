dca082cd88b3b039da60c8519ee704c2891c1a11
11249a150-art <11249a150@kanchiuniv.ac.in>
2026-04-20 20:13:56 +0530

    Update e_fibonacci.sh

--- EX_3_SHELL_PROGRAM/e_fibonacci.sh
+++ EX_3_SHELL_PROGRAM/e_fibonacci.sh
@@ -1,13 +1,4 @@
-#AIM: 
-#To write a shell program to display the Fibonacci series up to a given number of terms. 
- 
-#ALGORITHM: 
-#1. Read the number of terms. 
-#2. Initialize first two values as 0 and 1. 
-#3. Display the first two terms. 
-#4. Use a loop to generate remaining terms. 
-#5. Display the Fibonacci series. 
- 
+
 #PROGRAM  
 #!/bin/bash 
 echo "Enter number of terms:" 
@@ -24,26 +15,3 @@ do
   a=$b 
   b=$c 
 done
-
-
-#EXECUTION PROCEDURE: 
-#1. Create a file fibonacci.sh. 
-#2. Type the program and save it. 
-#3. Provide execute permission. 
-#4. Execute the program
- 
-#SAMPLE OUTPUT: 
- 
-#Enter number of terms: 
-#5 
-#Fibonacci Series: 
-#0 
-#1 
-#1 
-#2 
-#3 
- 
- 
- 
-#RESULT: 
-#Thus, the shell program to generate the Fibonacci series was executed successfully.
