b1fb4fdfb5e097b27cb2233e9b193342d0421f87
11249a150-art <11249a150@kanchiuniv.ac.in>
2026-04-20 20:16:59 +0530

    Update d_opendir_readdir.c

--- EX_5_DECISION_MAKING/d_opendir_readdir.c
+++ EX_5_DECISION_MAKING/d_opendir_readdir.c
@@ -1,14 +1,4 @@
-d) Program using opendir() and readdir() 
  
-Aim 
-To list files in a directory using directory system calls. 
- 
-Algorithm 
-1. Open directory using opendir(). 
-2. Read entries using readdir(). 
-3. Display file names. 
- 
-Source Code 
 #include <stdio.h> 
 #include <dirent.h> 
 int main() { 
@@ -21,7 +11,3 @@ int main() {
     closedir(dir); 
     return 0; 
 } 
- 
- 
-Result 
-Thus, directory contents were listed using opendir() and readdir().
