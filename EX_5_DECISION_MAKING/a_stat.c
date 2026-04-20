8de9e9e248c49f63136b653bac8508eb03b38129
11249a150-art <11249a150@kanchiuniv.ac.in>
2026-04-20 20:15:06 +0530

    Update a_stat.c

--- EX_5_DECISION_MAKING/a_stat.c
+++ EX_5_DECISION_MAKING/a_stat.c
@@ -1,16 +1,4 @@
-//AIM: 
-//To write and execute C programs using various UNIX/Linux system calls to understand 
-//file handling, process management, and directory operations. 
- 
-//a) Program using stat() System Call 
-//Aim 
-//To obtain file information using the stat() system call. 
- 
-//Algorithm 
-//1. Read file name from the user. 
-//2. Use stat() to retrieve file details. 
-//3. Display file size, inode number, and permissions. 
- 
+
 //Source Code 
 #include <stdio.h> 
 #include <sys/stat.h> 
@@ -25,16 +13,3 @@ int main() {
     printf("Permissions: %o\n", st.st_mode); 
     return 0; 
 } 
- 
- 
- 
-//Sample Output 
-//Enter file name: test.txt 
-//File size: 120 bytes 
-//Inode number: 256874 
-//Permissions: 100644 
- 
- 
- 
-//Result 
-//Thus, file information was successfully obtained using stat() system call.
